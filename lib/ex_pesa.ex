96909693c3e77ae35dd17b5560a1030e587bf18c
manuelgeek <emashmagak@appslab.co.ke>
2020-08-07 00:35:50 +0300

    initial commit

---
+++ lib/ex_pesa.ex
@@ -0,0 +1,18 @@
+defmodule ExPesa do
+  @moduledoc """
+  Documentation for `ExPesa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPesa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_pesa_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPesaTest do
+  use ExUnit.Case
+  doctest ExPesa
+
+  test "greets the world" do
+    assert ExPesa.hello() == :world
+  end
+end
