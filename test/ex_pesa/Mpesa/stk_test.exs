5b287d9b9511e07b0a8d85dfbe10980303c9e81c
manuelgeek <emashmagak@appslab.co.ke>
2020-08-26 21:24:49 +0300

    added stk transaction validation, and test case

--- test/ex_pesa/Mpesa/stk_test.exs
+++ test/ex_pesa/Mpesa/stk_test.exs
@@ -34,12 +34,26 @@ defmodule ExPesa.Mpesa.StkTest do
               "ResponseDescription" => "Success. Request accepted for processing"
             })
         }
+
+      %{url: "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query", method: :post} ->
+        %Tesla.Env{
+          status: 200,
+          body:
+            Jason.encode!(%{
+              "CheckoutRequestID" => "ws_CO_260820202102496165",
+              "MerchantRequestID" => "11130-78831728-4",
+              "ResponseCode" => "0",
+              "ResponseDescription" => "The service request has been accepted successsfully",
+              "ResultCode" => "1032",
+              "ResultDesc" => "Request cancelled by user"
+            })
+        }
     end)
 
     :ok
   end
 
-  describe "Mpesa STK Push" do
+  describe "Mpesa STK Push/ Validate Transaction" do
     test "request/1 should Initiate STK with required parameters" do
       request_details = %{
         amount: 10,
@@ -58,5 +72,18 @@ defmodule ExPesa.Mpesa.StkTest do
   test "request/1 should error out without required parameter" do
     {:error, result} = Stk.request(%{})
     "Required Parameters missing, 'phone, 'amount', 'reference', 'description'" = result
+  end
+
+  test "validate/1 should validate transaction successfully" do
+    {:ok, result} = Stk.validate(%{checkout_request_id: "ws_CO_260820202102496165"})
+
+    assert result["CheckoutRequestID"] == "ws_CO_260820202102496165"
+    assert result["ResponseCode"] == "0"
+    assert result["ResultDesc"] == "Request cancelled by user"
+  end
+
+  test "validate/1 should error out without required parameter" do
+    {:error, result} = Stk.validate(%{})
+    "Required Parameter missing, 'CheckoutRequestID'" = result
   end
 end
