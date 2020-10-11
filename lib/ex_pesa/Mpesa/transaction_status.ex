bca2b698e5d3d14027372d5a10a1390cbca8c060
kamalogudah <mcpaul2058@gmail.com>
2020-10-11 11:44:45 +0300

    Transaction Status

--- lib/ex_pesa/Mpesa/transaction_status.ex
+++ lib/ex_pesa/Mpesa/transaction_status.ex
@@ -4,66 +4,89 @@ defmodule ExPesa.Mpesa.TransactionStatus do
   """
 
   import ExPesa.Mpesa.MpesaBase
+  import ExPesa.Util
 
   @doc """
-  Initiates the Mpesa Lipa Online STK Push .
+  Transaction Status Query
+  ## Requirement Params
+  - `CommandID`[String] - Takes only 'TransactionStatusQuery' command id
+  - `PartyA` [Numeric] - Organization/MSISDN receiving the transaction, can be
+    -Shortcode (6 digits)
+    -MSISDN (12 Digits)
+  - `IdentifierType` [Numeric] - Type of organization receiving the transaction can be the folowing:
+      1 – MSISDN
+      2 – Till Number
+      4 – Organization short code
+  - `Remarks`[String] - Comments that are sent along with the transaction, can be a sequence of characters up to 100
+  - `Initiator` [Alpha-Numeric] - The name of Initiator to initiating  the request. This is the credential/username
+  used to authenticate the transaction request
+  - `SecurityCredential` [String] - Encrypted Credential of user getting transaction amount	String
+  Encrypted password for the initiator to authenticate the transaction request
+  - `QueueTimeOutURL` [URL] - The path that stores information of time out transaction. Takes the form of
+  https://ip or domain:port/path
+  - `ResultURL`[URL] - The path that stores information of transaction. Example https://ip or domain:port/path
+  - `TransactionID` [Alpha-Numeric] - Unique identifier to identify a transaction on M-Pesa	Alpha-Numeric	LKXXXX1234
+  -  `Occasion` [ String] -  Optional Parameter 	String	sequence of characters up to 100
 
-  ExPesa.Mpesa.TransactionStatus.query(%{command_id: "Command ID - TransactionStatusQuery",
-        identifier_type:  identifier_type,
-        remarks: "Some Remarks",
-        initiator: "kamalogudah",
-        security_credential: security_credential,
-        queue_timeout_url: queue_timeout_url,
-        result_url: result_url,
-        transaction_id: transaction_id,
-        occasion: "Some Occasion"})
 
-"TransactionID":"Transaction ID e.g LC7918MI73" ,
-"PartyA":"Phone number that initiated the transaction" ,
-"IdentifierType":"1" ,
-"ResultURL":"https://ip_address:port/result_url" ,
-"QueueTimeOutURL":"https://ip_address:port/timeout_url" ,
-"Remarks":"Remarks" ,
-"Occasion": "Optional parameter"
+  ## Parameters
+  The following are the parameters required for this method, the rest are fetched from config
+  files.
+  - `occasion`:
+  - `party_a`:
+  - `identifier_type``
+  - `remarks``
+  - `transaction_id`
+  Their details have been covered above in the documentation.
+
+  ## Example
+
+      iex> ExPesa.Mpesa.TransactionStatus.request(%{occasion: "Some Occasion",party_a: "600247",identifier_type: "4",remarks: "CustomerPayBillOnline",transaction_id: "SOME7803"})
+      {:ok,
+        %{
+            "ConversationID" => "AG_20201010_000056be35a7b266b43e",
+            "OriginatorConversationID" => "27288-72545279-2",
+            "ResponseCode" => "0",
+            "ResponseDescription" => "Accept the service request successfully."
+        }
+      }
 
   """
+  def request(params) do
+    case get_security_credential_for(:b2b) do
+      nil -> {:error, "cannot generate security_credential due to missing configuration fields"}
+      security_credential -> query(security_credential, params)
+    end
+  end
+
+  @doc false
+  def request() do
+    {:error,
+     "Some Required Parameter missing, check whether you have 'occasion', 'party_a', 'identifier_type', 'remarks',  and 'transaction_id'"}
+  end
+
   @spec request(map()) :: {:error, any()} | {:ok, any()}
-  def query(%{
-        command_id: command_id,
-        remarks: remarks,
-        initiator: initiator,
-        security_credential: security_credential,
-        queue_timeout_url: queue_timeout_url,
-        result_url: result_url,
-        transaction_id: transaction_id,
-        occasion: occasion
-
-      }) do
-    paybill = Application.get_env(:ex_pesa, :mpesa)[:mpesa_short_code]
-    passkey = Application.get_env(:ex_pesa, :mpesa)[:mpesa_passkey]
-    {:ok, timestamp} = Timex.now() |> Timex.format("%Y%m%d%H%M%S", :strftime)
-    password = Base.encode64(paybill <> passkey <> timestamp)
-    security_credential = Base.encode64(password)
-
+  defp query(security_credential, %{
+         occasion: occasion,
+         transaction_id: transaction_id,
+         party_a: party_a,
+         identifier_type: identifier_type,
+         remarks: remarks
+       }) do
     payload = %{
-      "CommandID" => command_id,
-      "ShortCode" =>  paybill,
-      "IdentifierType" => "1",
-      "Remarks" => "CustomerPayBillOnline",
+      "CommandID" => "TransactionStatusQuery",
+      "PartyA" => party_a,
+      "IdentifierType" => identifier_type,
+      "Remarks" => remarks,
       "SecurityCredential" => security_credential,
-      "Initiator" => amount,
-      "SecurityCredential" => phone,
-      "QueueTimeOutURL" => paybill,
-      "ResultURL" => phone,
-      "TransactionID" => Application.get_env(:ex_pesa, :mpesa)[:mpesa_callback_url],
+      "Initiator" => Application.get_env(:ex_pesa, :mpesa)[:transaction_status][:initiator_name],
+      "QueueTimeOutURL" =>
+        Application.get_env(:ex_pesa, :mpesa)[:transaction_status][:timeout_url],
+      "ResultURL" => Application.get_env(:ex_pesa, :mpesa)[:transaction_status][:result_url],
+      "TransactionID" => transaction_id,
       "Occasion" => occasion
     }
 
     make_request("/mpesa/transactionstatus/v1/query", payload)
   end
-
-  def query(_) do
-    {:error, "Required Parameters missing, 'CommandID, 'IdentifierType', 'Remarks', 'Initiator'"}
-  end
-
 end
---
+++ test/ex_pesa/Mpesa/transaction_status_test.exs
@@ -0,0 +1,65 @@
+defmodule ExPesa.Mpesa.TransactionStatusTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  import Tesla.Mock
+  doctest ExPesa.Mpesa.TransactionStatus
+
+  alias ExPesa.Mpesa.TransactionStatus
+
+  setup do
+    mock(fn
+      %{
+        url: "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+        method: :get
+      } ->
+        %Tesla.Env{
+          status: 200,
+          body: %{
+            "access_token" => "SGWcJPtNtYNPGm6uSYR9yPYrAI3Bm",
+            "expires_in" => "3599"
+          }
+        }
+
+      %{
+        url: "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query",
+        method: :post
+      } ->
+        %Tesla.Env{
+          status: 200,
+          body: %{
+            "ConversationID" => "AG_20201010_000056be35a7b266b43e",
+            "OriginatorConversationID" => "27288-72545279-2",
+            "ResponseCode" => "0",
+            "ResponseDescription" => "Accept the service request successfully."
+          }
+        }
+    end)
+
+    :ok
+  end
+
+  describe "Mpesa Transaction Status" do
+    test "request/1 should Initiate a B2B request" do
+      query_details = %{
+        occasion: "Some Occasion",
+        party_a: "600247",
+        identifier_type: "4",
+        remarks: "CustomerPayBillOnline",
+        transaction_id: "SOME7803"
+      }
+
+      {:ok, result} = TransactionStatus.request(query_details)
+
+      assert result["ResponseDescription"] == "Accept the service request successfully."
+    end
+
+    test "request/1 should error out without required parameter" do
+      {:error, result} = TransactionStatus.request()
+
+      "Some Required Parameter missing, check whether you have 'occasion', 'party_a', 'identifier_type', 'remarks',  and 'transaction_id'" =
+        result
+    end
+  end
+end
